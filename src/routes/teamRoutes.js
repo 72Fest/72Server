4b5490cc8feda80624830565139fe6aa8660ce50
Howell <github@foo.ro>
2016-09-22 22:50:50 -0400

    debugging json output for teams

--- src/routes/teamRoutes.js
+++ src/routes/teamRoutes.js
@@ -2,26 +2,22 @@ var express = require('express');
 var teamRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
 var objectId = require('mongodb').ObjectID;
-
+var url = 'mongodb://localhost:27017/72Fest';
 teamRouter.route('/')
     .get(function (req, res) {
-        var url =
-            'mongodb://localhost:27017/72Fest';
+        
         mongodb.connect(url, function (err, db) {
             var collection = db.collection('teams');
             collection.find({}).sort('teamName', 1).toArray(
                 function (err, results) {
-                    //res.json(results);
-                    res.render('teamListView', {
-                        title: 'Teams',
-                        teams: results
-                    });
+                    res.json(results);
+//                    res.render('teamListView', {
+//                        title: 'Teams',
+//                        teams: results
+//                    });
                 });
         });
-//        res.render('teamListView',{
-//            title: 'Teams',
-//            teams: results
-//        });
+
     });
 
 module.exports = teamRouter
