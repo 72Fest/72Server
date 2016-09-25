ad4e3b2815b757cc6417fdd43fb6a80de19ea6f4
Howell <github@foo.ro>
2016-09-25 19:30:17 -0400

    team adding and editing minus logos

--- src/routes/newsRoutes.js
+++ src/routes/newsRoutes.js
@@ -3,88 +3,93 @@ var newsRouter = express.Router();
 var markdown = require('markdown').markdown;
 var mongodb = require('mongodb').MongoClient;
 var objectId = require('mongodb').ObjectID;
- var url ='mongodb://localhost:27017/72Fest';
+var url = 'mongodb://localhost:27017/72Fest';
 var router = function () {
-        newsRouter.route('/add')
-            .get(function (req, res) {
-                res.render('newsAddView');
-            });
+    newsRouter.route('/add')
+        .get(function (req, res) {
+            res.render('newsAddView');
+        });
 
-        newsRouter.route('/')
-            .post(function (req, res) {
-              
-                mongodb.connect(url, function (err, db) {
-                    console.log(req.body);
-                    var collection = db.collection('news');
-                    var newsItem = {
-                        title: req.body.newsTitle,
-                        content: req.body.article,
-                        timestamp: new Date()
-                    };
-                    collection.insert(newsItem, function (err, results) {
+    newsRouter.route('/')
+        .post(function (req, res) {
 
-                        res.redirect('/news');
+            mongodb.connect(url, function (err, db) {
+                console.log(req.body);
+                var collection = db.collection('news');
+                var newsItem = {
+                    title: req.body.newsTitle,
+                    content: req.body.article,
+                    timestamp: new Date()
+                };
+                collection.insert(newsItem, function (err, results) {
 
-                    });
+                    res.redirect('/news');
 
                 });
-                console.log('did it');
-            })
 
+            });
+            console.log('did it');
+        })
+
+    .get(function (req, res) {
+
+        mongodb.connect(url, function (err, db) {
+            var collection = db.collection('news');
+            collection.find({}).toArray(
+                function (err, results) {
+                    results.forEach(function (result) {
+                        result.content = markdown.toHTML(result.content);
+                    });
+                    //console.log(results);
+                    //res.json(results);
+                    res.render('newsListView', {
+                        title: 'News',
+                        news: results
+                    });
+                });
+        });
+    });
+
+    newsRouter.route('/:id')
         .get(function (req, res) {
+
+            var id = new objectId(req.params.id);
 
             mongodb.connect(url, function (err, db) {
                 var collection = db.collection('news');
-                collection.find({}).toArray(
+                collection.findOne({
+                        _id: id
+                    },
                     function (err, results) {
-                        results.forEach(function (result) {
-                            result.content = markdown.toHTML(result.content);
-                        });
-                        //console.log(results);
-                        //res.json(results);
-                        res.render('newsListView', {
+                        res.render('newsEditView', {
                             title: 'News',
                             news: results
                         });
                     });
             });
-        });
+        })
+        .put(function (req, res) {
+            console.log('put');
+            var id = new objectId(req.params.id);
 
-        newsRouter.route('/:id')
-            .get(function (req, res) {
-
-                var id = new objectId(req.params.id);
-
-                mongodb.connect(url, function (err, db) {
-                    var collection = db.collection('news');
-                    collection.findOne({
-                            _id: id
-                        },
-                        function (err, results) {
-                            res.render('newsEditView', {
-                                title: 'News',
-                                news: results
-                            });
-                        });
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('news');
+                collection.findOne({
+                    _id: id
+                }, function (err, result) {
+                    result.title = req.body.newsTitle;
+                    result.content = req.body.article;
+                    collection.updateOne({
+                        _id: id
+                    }, result);
+                    //                            collection.updateOne({_id : id},{$set : {title : req.body.newsTitle, content : req.body.article }});
+                    res.redirect('/news');
                 });
-            })
-            .put(function (req, res) {
-                    console.log('put');
-                    var id = new objectId(req.params.id);
-                   
-                    mongodb.connect(url, function (err, db) {
-                        var collection = db.collection('news');
-                         collection.findOne({
-                            _id: id
-                        }, function (err, result) {
-                            
-                            collection.updateOne({_id : id},{$set : {title : req.body.newsTitle, content : req.body.article }});
-                           res.redirect('/news');
-                        });
 
 
-                    }); });
-                    return newsRouter;
-                };
+            });
+        });
+    return newsRouter;
+};
 
-                module.exports = router;
+module.exports = router;
--- src/routes/teamRoutes.js
+++ src/routes/teamRoutes.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var formidable = require('formidable');
 var teamRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
 var objectId = require('mongodb').ObjectID;
@@ -18,6 +19,37 @@ teamRouter.route('/')
                 });
         });
 
+    })
+    .post(function (req, res) {
+        mongodb.connect(url, function (err, db) {
+            var collection = db.collection('teams');
+            var team = {
+                teamName: req.body.teamName,
+                bio: req.body.bio,
+                films: new Array()
+            }
+            collection.insert(team, function (err, results) {
+                res.redirect('/teams/' + results.ops[0]._id);
+            })
+        });
+    });
+
+teamRouter.route('/upload')
+    .get(function (req, res) {
+        res.render('upload', {
+            title: 'Upload'
+        });
+    })
+    .post(function (req, res) {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+            res.send(files)
+        })
+
+    });
+teamRouter.route('/add')
+    .get(function (req, res) {
+        res.render('teamAddView');
     });
 
 teamRouter.route('/:id')
@@ -37,6 +69,71 @@ teamRouter.route('/:id')
                     });
                 });
         });
+    })
+    .put(function (req, res) {
+        var id = new objectId(req.params.id);
+        mongodb.connect(url, function (err, db) {
+            var collection = db.collection('teams');
+            collection.findOne({
+                _id: id
+            }, function (err, result) {
+                result.teamName = req.body.teamName;
+                result.bio = req.body.bio;
+                var filmArray = new Array();
+
+                if(result.films.length > 0){
+                    if(Array.isArray(req.body.filmTitle))
+                    {
+                                       for (var i = 0; i < req.body.filmTitle.length; i++) {
+                    result.films[i].title = req.body.filmTitle[i];
+                    result.films[i].year = req.body.filmYear[i];
+                    result.films[i].url = req.body.filmUrl[i];
+                }
+                    }
+                else{
+                    result.films[0].title = req.body.filmTitle;
+                    result.films[0].year = req.body.filmYear;
+                    result.films[0].url = req.body.filmUrl;
+                }
+                }
+                
+
+                collection.updateOne({
+                    _id: id
+                }, result);
+                res.redirect('/teams/' + id);
+            });
+
+
+        });
     });
+
+teamRouter.route('/:id/films')
+    .post(function (req, res) {
+        var id = new objectId(req.params.id);
+        mongodb.connect(url, function (err, db) {
+            var collection = db.collection('teams');
+            collection.findOne({
+                _id: id
+            }, function (err, result) {
+                console.log(result);
+                var film = {
+                    title: req.body.filmTitle,
+                    year: req.body.filmYear,
+                    url: req.body.filmUrl
+                };
+                result.films.push(film);
+                collection.updateOne({
+                    _id: id
+                }, result);
+                res.redirect('/teams/' + id);
+            });
+
+
+        });
+    });
+
+
+
 
 module.exports = teamRouter
